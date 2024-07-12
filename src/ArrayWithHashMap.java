68ba46d9437916a2596bee1b6c9d87c51678bc4b
Sandesh Sarade <Saradesandesh@gmail.com>
2024-07-12 13:41:02 +0530

    Arrays ids done sucessfully..

---
+++ src/ArrayWithHashMap.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ArrayWithHashMap {
+ public static void main(String[] args) {
+  
+  HashMap<String, ArrayList<String>> hashMap = new HashMap<>();
+
+  // Create an ArrayList of Strings
+  ArrayList<String> list1 = new ArrayList<>();
+  list1.add("Alice");
+  list1.add("Bob");
+  list1.add("Charlie");
+
+  // Create another ArrayList of Strings
+  ArrayList<String> list2 = new ArrayList<>();
+  list2.add("David");
+  list2.add("Eve");
+  list2.add("Frank");
+
+  // Put the ArrayLists into the HashMap
+  hashMap.put("Group1", list1);
+  hashMap.put("Group2", list2);
+
+  // Print the HashMap
+  for (String key : hashMap.keySet()) {
+      System.out.println("Key: " + key + ", Value: " + hashMap.get(key));
+  }
+ }
+}
---
+++ src/IterteHashMap.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class IterteHashMap {
+  public static void main(String[] args) {
+    HashMap <String, Integer> map = new HashMap<>();
+    map.put("one", 1);
+    map.put("two", 2);
+    map.put("three", 3);
+    map.put("four", 4);
+    map.put("five", 5);
+
+    // varriuys way it iterate the hashmap
+
+    // by using entryset()
+   for (Map.Entry<String, Integer> keyss: map.entrySet()) {
+    System.out.println("Key is "+keyss+ " values is "+ keyss.getValue());
+    
+   }
+   System.out.println(" ");
+   // by using key set
+
+   for (String  keysss : map.keySet()) {
+    System.out.println("Key is "+keysss+ " values is "+ map.get(keysss));   
+   }
+   System.out.println(" ");
+   for (Integer string : map.values()) {
+    System.out.println(string);
+    
+   }
+    System.out.println(" ");
+   map.forEach((key,value)->{
+      System.out.println("key is "+key + " value is "+value);
+   });
+
+   // while using iterator
+
+     
+  }
+}
