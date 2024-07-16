863ae7210eceed60fc76bb32e2302c61e92d7fa8
Sandesh Sarade <Saradesandesh@gmail.com>
2024-07-16 22:44:14 +0530

    changing modification

--- src/NewCodeGeneration/main.java
+++ src/NewCodeGeneration/main.java
@@ -1,7 +1,14 @@
 package NewCodeGeneration;
 
 public class main {
-  public static void main(String[] args) {
-    System.out.println("hello");
+  public static void main (String[] args) {
+    for(int i=5;i>=5;i--){
+      for(int j=5;j>=1;j--){
+        System.out.print(" *");
+      }
+     System.out.println(" ");
+    }
+    
+    }
   }
-}
+
